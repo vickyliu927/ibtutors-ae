840fef42fe9edfb9c463ad82cbdda875eb4990fc
Jingru Liu <jingruliu@Jingrus-MacBook-Air.local>
2025-05-04 13:56:16 +0100

    feat: Add Sanity data fetching for hero, tutors, and testimonials sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,42 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import HeroSection from './components/HeroSection';
-import TutorProfiles from './components/TutorProfiles';
+import HeroSection, { HeroData } from './components/HeroSection';
+import TutorProfiles, { TutorData } from './components/TutorProfiles';
 import SubjectGrid from './components/SubjectGrid';
 import TutoringPlatformBanner from './components/TutoringPlatformBanner';
-import TestimonialSection from './components/TestimonialSection';
+import TestimonialSection, { TestimonialData, TestimonialSectionData } from './components/TestimonialSection';
 import FAQ from './components/FAQ';
 import ContactForm from './components/ContactForm';
 import Footer from './components/Footer';
+import { client } from '@/sanity/lib/client';
 
-export default function Home() {
+async function getHomePageData() {
+  const heroQuery = `*[_type == "hero"][0]`;
+  const tutorsQuery = `*[_type == "tutor"] | order(order asc)`;
+  const testimonialSectionQuery = `*[_type == "testimonial_section"][0]`;
+  const testimonialsQuery = `*[_type == "testimonial"] | order(order asc)`;
+
+  const [heroData, tutors, testimonialSection, testimonials] = await Promise.all([
+    client.fetch<HeroData>(heroQuery),
+    client.fetch<TutorData[]>(tutorsQuery),
+    client.fetch<TestimonialSectionData>(testimonialSectionQuery),
+    client.fetch<TestimonialData[]>(testimonialsQuery),
+  ]);
+
+  return { heroData, tutors, testimonialSection, testimonials };
+}
+
+export default async function Home() {
+  const { heroData, tutors, testimonialSection, testimonials } = await getHomePageData();
+
   return (
     <main>
       <Navbar />
-      <HeroSection />
-      <TutorProfiles />
+      <HeroSection heroData={heroData} />
+      <TutorProfiles tutors={tutors} />
       <SubjectGrid />
       <TutoringPlatformBanner />
-      <TestimonialSection />
+      <TestimonialSection sectionData={testimonialSection} testimonials={testimonials} />
       <FAQ />
       <ContactForm />
       <Footer />
