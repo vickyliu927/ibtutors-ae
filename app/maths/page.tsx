870e5e7fcca1d8300ce7e5d70c2d9923b8a92ca1
Jingru Liu <jingruliu@Jingrus-MacBook-Air.local>
2025-05-04 13:26:29 +0100

    chore: add temporary static maths page for successful build and deploy

--- app/maths/page.tsx
+++ app/maths/page.tsx
@@ -1,54 +1,16 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-import HeroSection, { HeroData } from '../components/HeroSection';
-import TutorProfiles, { TutorData } from '../components/TutorProfiles';
-import TestimonialSection, { TestimonialSectionData, TestimonialData } from '../components/TestimonialSection';
 import ContactForm from '../components/ContactForm';
 import Footer from '../components/Footer';
-import { client } from '@/sanity/lib/client';
 
-async function getMathsPageData() {
-  const query = `*[_type == "subjectPage" && slug.current == "maths"][0]{
-    hero,
-    tutorProfiles[]->{
-      _id,
-      name,
-      professionalTitle,
-      education,
-      experience,
-      profilePhoto,
-      specialization,
-      yearsOfExperience,
-      hireButtonLink,
-      subjects,
-      price
-    },
-    testimonialSection->{
-      rating,
-      totalReviews,
-      subtitle,
-      tutorChaseLink
-    },
-    "testimonials": *[_type == "testimonial" && references(^.testimonialSection._ref)] | order(order asc) {
-      _id,
-      reviewerName,
-      reviewerType,
-      testimonialText,
-      rating,
-      order
-    }
-  }`;
-  return await client.fetch(query);
-}
-
-export default async function MathsPage() {
-  const data = await getMathsPageData();
+export default function MathsPage() {
   return (
     <main>
       <Navbar />
-      <HeroSection heroData={data.hero as HeroData} />
-      <TutorProfiles tutors={data.tutorProfiles as TutorData[]} />
-      <TestimonialSection sectionData={data.testimonialSection as TestimonialSectionData} testimonials={data.testimonials as TestimonialData[]} />
+      <section className="py-32 text-center">
+        <h1 className="text-5xl font-bold mb-6">IB Maths Tutors</h1>
+        <p className="text-xl text-gray-600 mb-8">This is a temporary static page for IB Maths. Dynamic content will be added soon.</p>
+      </section>
       <ContactForm />
       <Footer />
     </main>
