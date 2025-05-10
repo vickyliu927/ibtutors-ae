395daefd3314981c215012d47901307c2c1624e0
Jingru Liu <jingruliu@Jingrus-MacBook-Air.local>
2025-05-10 18:44:13 +0100

    Fix TypeError: Add null checks for tutor education data

--- app/components/TutorProfiles.tsx
+++ app/components/TutorProfiles.tsx
@@ -153,7 +153,9 @@ const TutorProfiles = ({
                         <path d="M12 3L1 9l11 6 9-4.91V17h2V9M5 13.18v4L12 21l7-3.82v-4L12 17l-7-3.82z" />
                       </svg>
                       <p className="text-black font-medium">
-                        {tutor.specialization.mainSubject} | {tutor.education.university} | {tutor.education.degree}
+                        {tutor.specialization.mainSubject}
+                        {tutor.education && tutor.education.university && ` | ${tutor.education.university}`}
+                        {tutor.education && tutor.education.degree && ` | ${tutor.education.degree}`}
                       </p>
                     </div>
                   </div>
