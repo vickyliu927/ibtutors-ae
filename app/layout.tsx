b95d9bcd71074deb18051429dcffa2461f9372d0
Jingru Liu <vicliu927@gmail.com>
2025-10-28 23:11:36 +0000

    added google ads tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -83,6 +84,19 @@ export default async function RootLayout({
             }),
           }}
         />
+        {/* Google tag (gtag.js) for GA4 */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-84X27600SD"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-84X27600SD');
+          `}
+        </Script>
       </head>
       <body className={inter.className}>
         <DebuggerInitializer />
