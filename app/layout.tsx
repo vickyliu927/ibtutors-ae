3f94424f5d9fa8a648292ab7d4198acc65d76ecd
Jingru Liu <vicliu927@gmail.com>
2025-11-26 22:42:55 +0000

    added GTM container and removed old one

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import Script from 'next/script';
 import './globals.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import DebuggerInitializer from './components/DebuggerInitializer';
+import { GTMHead, GTMNoScript } from './components/GTM';
 import { getNavigationData } from './lib/getNavigationData';
 import { getSeoData } from './lib/getSeoData';
 import { getCurrentDomainFromHeaders, getCanonicalDomain } from './lib/sitemapUtils';
@@ -60,31 +60,9 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google tag (gtag.js) - GA4 G-H9E5WDK5MX */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-H9E5WDK5MX"
-          strategy="afterInteractive"
-        />
-        <Script id="gtag-init" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-H9E5WDK5MX');
-          `}
-        </Script>
         <meta name="google-site-verification" content="v4S2fecY05CWeIbQ6FvCG-5LZv2FvTJa56JfscMhS_Y" />
-        {/* Removed render-blocking Google Fonts for Gilroy; Inter is provided via next/font */}
-            {/* Google Tag Manager */}
-            <Script id="gtm-loader" strategy="afterInteractive">
-              {`
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-N5624CFQ');
-              `}
-            </Script>
+        {/* Google Tag Manager - global for all tenants */}
+        <GTMHead />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
@@ -110,14 +88,7 @@ export default async function RootLayout({
       </head>
       <body className={inter.className}>
         {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-N5624CFQ"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
-        </noscript>
+        <GTMNoScript />
         <DebuggerInitializer />
         <Navbar 
           navbarData={navigationData.navbarData}
