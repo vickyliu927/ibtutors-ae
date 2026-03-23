ded0bc3bcdad04e7332094d779e36b9de24d61fd
Jingru Liu <vicliu927@gmail.com>
2026-03-23 12:13:48 +0000

    added tiktok pixel code

--- app/layout.tsx
+++ app/layout.tsx
@@ -106,6 +106,35 @@ export default async function RootLayout({
             alt=""
           />
         </noscript>
+        {/* TikTok Pixel - TutorChase Pixel (CU379URC77UC5BF3HD80) */}
+        <Script id="tiktok-pixel" strategy="afterInteractive">{`
+          !function (w, d, t) {
+            w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(
+            var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script")
+            ;n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+            ttq.load('CU379URC77UC5BF3HD80');
+            ttq.page();
+          }(window, document, 'ttq');
+        `}</Script>
+        {/* TikTok Pixel - SPA navigation tracking */}
+        <Script id="tiktok-pixel-route-events" strategy="afterInteractive">{`
+          (function() {
+            var fire = function() { try { if (window.ttq) window.ttq.page(); } catch (_) {} };
+            var origPushState = history.pushState;
+            history.pushState = function() {
+              var ret = origPushState.apply(this, arguments);
+              fire();
+              return ret;
+            };
+            var origReplaceState = history.replaceState;
+            history.replaceState = function() {
+              var ret = origReplaceState.apply(this, arguments);
+              fire();
+              return ret;
+            };
+            window.addEventListener('popstate', fire);
+          })();
+        `}</Script>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
